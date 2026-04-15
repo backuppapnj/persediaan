7d64ace095cb63c28fcaa2ed219b17dda6a2ed80
Moohard <moohard@example.com>
2026-04-16 00:20:06 +0800

    refactor: migrate Stok_model to PDO

--- app/modules/kartustok/models/Stok_model.php
+++ app/modules/kartustok/models/Stok_model.php
@@ -3,11 +3,7 @@ require_once APP_PATH . '/core/Model.php';
 
 class Stok_model extends Model {
     public function getLogByBarangId($id_barang) {
-        $stmt = $this->db->prepare("SELECT * FROM tbl_log_stok WHERE id_barang = ? ORDER BY tanggal_log DESC");
-        $stmt->bind_param("i", $id_barang);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        return $result->fetch_all(MYSQLI_ASSOC);
+        return $this->fetchAll("SELECT * FROM tbl_log_stok WHERE id_barang = :id_barang ORDER BY tanggal_log DESC", ['id_barang' => $id_barang]);
     }
 }
 ?>
